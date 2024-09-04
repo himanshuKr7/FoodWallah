050f88b4255ad639dae61ee071c297f61a9da988
Himanshu Kumar <himanshu7277069@gmail.com>
2024-09-04 18:22:25 +0530

    small changes

--- src/server/app.js
+++ src/server/app.js
@@ -8,7 +8,12 @@ const port = 3001;
 
 const FOODWALLAH_MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
 
-app.use(cors());
+app.use(cors({
+    origin:"https://food-wallah-ten.vercel.app",
+    methods:["GET","POST","DELETE","PUT"],
+    credentials:false
+}
+));
 
 app.get('/api/restaurants', async (req, res) => {
     const url = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING';
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_API_KEY,
-  authDomain:process.env.REACT_APP_AUTHDOMAIN,
-  projectId:process.env.REACT_APP_PROJECTID,
-  storageBucket:process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
-  appId:process.env.REACT_APP_APPID
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTHDOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECTID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGINGSENDERID}`,
+  appId: `${process.env.REACT_APP_APPID}`,
 };
 
 // Initialize Firebase
